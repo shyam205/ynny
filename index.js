4b18734a67468031a2417bbaaa321a86fa904c61
shyam205 <mastershyam2013@gmail.com>
2021-03-01 15:03:40 +0530

    added second banner

--- index.js
+++ index.js
@@ -7,12 +7,27 @@ $(document).ready(function() {
 		});              
      }
 	 if(window.innerWidth < 769){
-		$('#toppage2').ripples({
+		$('.container').ripples({
 				resolution: 256,
 				dropRadius: 10, 
 				perturbance: 0.01,
 			}); 
 		}
+		if(window.innerWidth > 768){
+			$('.second-banner').ripples({
+					resolution: 512,
+					dropRadius: 20, 
+					perturbance: 0.04,
+				});              
+			 }
+			 if(window.innerWidth < 769){
+				$('.second-banner').ripples({
+						resolution: 256,
+						dropRadius: 10, 
+						perturbance: 0.01,
+					}); 
+				}
+
   $("#lable").click(function(){
 	  $(".second-banner").toggleClass('banner-second');
   })
