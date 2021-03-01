8f54e037fa967ab55ef25faa64838535424d79f4
shyam205 <mastershyam2013@gmail.com>
2021-03-01 13:47:45 +0530

    added few styles

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ $(document).ready(function() {
 			perturbance: 0.04,
 		});              
      }
+	 if(window.innerWidth < 769){
+		$('#toppage2').ripples({
+				resolution: 256,
+				dropRadius: 10, 
+				perturbance: 0.01,
+			}); 
+		}
   $("#lable").click(function(){
 	  $(".second-banner").toggleClass('banner-second');
   })
